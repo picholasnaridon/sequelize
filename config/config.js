eee5832f9dfa082cf281fb136d664c2a0d9820d7
nick paridon <paridonn1@gmail.com>
2018-08-15 19:30:08 -0400

    refactor DB config

---
+++ config/config.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+module.exports = {
+  development: {
+    username: "root",
+    password: "",
+    database: "burger_dev",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql'
+
+  }
+}
